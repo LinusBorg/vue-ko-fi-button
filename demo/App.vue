e484983d4930b34819085ca8b6011e055e9c84af
Thorsten <t.luenborg@googlemail.com>
2019-04-10 21:08:42 +0200

    fix demo

--- demo/App.vue
+++ demo/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png" />
     <br />
-    <KoFiButton title="Givese me Moneys!" color="#CCC" id="123" />
+    <KoFiButton title="Givese me Moneys!" color="#CCC" username="linusborg" />
   </div>
 </template>
 
